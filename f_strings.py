2984eb84564dcbaa887345295572fd4ab1bae173
Darshan Halani <141173397+darshanhalani18@users.noreply.github.com>
2024-11-13 23:20:06 +0530

    initial program

---
+++ f_strings.py
@@ -0,0 +1,10 @@
+name = 'Darshan'
+age = 19
+
+print("My Name is " + name + " and I'm " + str(age) + " years old.")
+
+print("My Name is ", name, "and I'm", age, "years old.")
+
+# Using f-string...(Simple and Convenient way for String formatting).
+
+print(f"My Name is {name} and I'm {age} years old.")
